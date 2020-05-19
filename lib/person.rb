403b0a78bfbbcde5f8f99e5fb20bf39afa3c1310
Dustin Blanton <dustinblntn@gmail.com>
2020-05-19 21:54:57 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,30 @@
+class Person
+  def instance(name)
+    @name = name
+  end
+  
+  def name 
+    @name
+  end
+  
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  def job(job)
+    @job = job
+  end
+  
+  def job
+    @job
+  end
+  
+  def job=(new_job)
+    @job = new_job
+  end
+  
+
+
+  
+end
+
