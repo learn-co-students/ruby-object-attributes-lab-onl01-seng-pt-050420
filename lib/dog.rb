18021e1caac174f9826e7d0f9e0f3cc01d1da2c2
Victor Williams <victor.williams1030@gmail.com>
2020-05-19 12:52:29 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+  
+  def name= (doggy_name)
+    @name = doggy_name
+  end
+  
+  def name 
+    @name
+  end
+
+  def breed= (doggy_breed)
+    @breed = doggy_breed
+  end
+  
+  def breed
+    @breed
+  end
+  
+end
---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person 
+  def name= (famous_name)
+    @name = famous_name
+  end
+  
+  def name 
+    @name
+  end
+  
+  def job= (person_job)
+    @job = person_job
+  end
+  
+  def job 
+    @job
+  end
+end 
+
